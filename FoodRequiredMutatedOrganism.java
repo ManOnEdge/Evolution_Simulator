eac7b3d650250353226eb95e975166ad9db95ae5
ManOnEdge <mruss797@gmail.com>
2019-12-03 12:38:57 +0000

    Update FoodRequiredMutatedOrganism.java

--- FoodRequiredMutatedOrganism.java
+++ FoodRequiredMutatedOrganism.java
@@ -1,6 +1,7 @@
 package sample;
 
 public class FoodRequiredMutatedOrganism extends Organism{
+    
     public FoodRequiredMutatedOrganism( int size, int speed, int foodRequired, int x, int y) {
         super ( size, speed, foodRequired, x, y);
     }
