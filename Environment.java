4a105af441959353c41d4c3b72257071fd52f76d
ManOnEdge <mruss797@gmail.com>
2019-12-03 12:33:38 +0000

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -2,12 +2,4 @@ package sample;
 
 public class Environment {
     int environmentSize;
-
-    public int getEnvironmentSize() {
-        return environmentSize;
-    }
-
-    public void setEnvironmentSize(int environmentSize) {
-        this.environmentSize = environmentSize;
-    }
 }
