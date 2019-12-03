c550e0a1c946628f095a3605447f2d03d1027739
ManOnEdge <mruss797@gmail.com>
2019-12-03 12:34:13 +0000

    Update Organism.java

--- Organism.java
+++ Organism.java
@@ -12,28 +12,4 @@ public class Organism {
         this.speed = speed;
         this.foodRequired = foodRequired;
     }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public int getFoodRequired() {
-        return foodRequired;
-    }
-
-    public void setFoodRequired(int foodRequired) {
-        this.foodRequired = foodRequired;
-    }
 }
