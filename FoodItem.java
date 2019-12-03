3a3a6c5c46eadabb36e6f03a077bc51efdc4c43e
ManOnEdge <mruss797@gmail.com>
2019-12-03 12:39:28 +0000

    Update FoodItem.java

--- FoodItem.java
+++ FoodItem.java
@@ -5,15 +5,10 @@ public class FoodItem {
     int x;
     int y;
 
-    public FoodItem(int startingFoodNumber){
+    public FoodItem(int startingFoodNumber, int x, int y)
+    {
         this.startingFoodNumber = startingFoodNumber;
-    }
-
-    public int getStartingFoodNumber() {
-        return startingFoodNumber;
-    }
-
-    public void setStartingFoodNumber(int startingFoodNumber) {
-        this.startingFoodNumber = startingFoodNumber;
+        this.x = x;
+        this.y = y;
     }
 }
