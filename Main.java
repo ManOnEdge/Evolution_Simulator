f4944d976286aa5694435132d86abf0db9bf3505
ManOnEdge <mruss797@gmail.com>
2019-12-07 18:27:56 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,35 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Parent;
+        import javafx.scene.Scene;
+        import javafx.scene.layout.AnchorPane;
+        import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("canvas.fxml"));
+        AnchorPane rootLayout = loader.load();
+
+        Scene scene = new Scene(rootLayout, 500, 500);
+        primaryStage.setScene(scene);
+
+        CanvasController controller = loader.getController();
+        controller.tick ();
+
+        Parent root = FXMLLoader.load(getClass().getResource("canvas.fxml"));
         primaryStage.setTitle("Evolution Simulator");
         primaryStage.setScene(new Scene(root, 500, 550));
         primaryStage.show();
+
     }
 
-
+    
     public static void main(String[] args) {
         launch(args);
     }
