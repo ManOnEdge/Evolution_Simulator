49f2d2afa23da7551df9c22d01b2ad379979d7ee
ManOnEdge <mruss797@gmail.com>
2019-12-07 18:28:23 +0000

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -1,5 +1,23 @@
 package sample;
 
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Environment {
-    int environmentSize;
+    int height;
+    int width;
+    public void tick()
+    {
+        while(true){
+            System.out.println ("++");
+            try{
+                Thread.sleep(100);
+            }
+            catch(InterruptedException e){
+                System.out.println(e);
+            }
+        }
+        //for loop to update every organism
+    }
 }
