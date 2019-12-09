8e2c6f6060931f0b8b814e9f48a2a0e7b0ac8a50
ManOnEdge <mruss797@gmail.com>
2019-12-09 17:09:07 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,36 +1,53 @@
 package sample;
 
-        import javafx.application.Application;
-        import javafx.fxml.FXMLLoader;
-        import javafx.scene.Parent;
-        import javafx.scene.Scene;
-        import javafx.scene.layout.AnchorPane;
-        import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
+    public static final int Width = 500;
+    public static final int Height = 500;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("canvas.fxml"));
-        AnchorPane rootLayout = loader.load();
+        GridPane sampleRoot = FXMLLoader.load (getClass ().getResource ("sample.fxml"));
+        primaryStage.setTitle ("Evolution Simulator");
+        Scene sampleScene = new Scene (sampleRoot, 500, 550);
+        primaryStage.setScene (sampleScene);
+        primaryStage.show ();
 
-        Scene scene = new Scene(rootLayout, 500, 500);
-        primaryStage.setScene(scene);
+        Button butRun = (Button) sampleScene.lookup ("#butRun");
+        butRun.setOnAction (event -> {
+            try {
+                AnchorPane canvasRoot = FXMLLoader.load (getClass ().getResource ("canvas.fxml"));
+                Scene scene = new Scene (canvasRoot, Width, Height);
+                primaryStage.setScene (scene);
+                primaryStage.show ();
 
-        CanvasController controller = loader.getController();
-        controller.tick ();
+            } catch (IOException e) {
+                e.printStackTrace ();
+            }
+        });
 
-        Parent root = FXMLLoader.load(getClass().getResource("canvas.fxml"));
-        primaryStage.setTitle("Evolution Simulator");
-        primaryStage.setScene(new Scene(root, 500, 550));
-        primaryStage.show();
+        /*AnchorPane canvasRoot = FXMLLoader.load (getClass ().getResource ("canvas.fxml"));
+
+        Scene scene = new Scene (canvasRoot, Width, Height);
+        primaryStage.setScene (scene);
+        primaryStage.show ();*/
+//        CanvasController controller = loader.getController ();
+//        Environment environment = new Environment (10, Width, Height);
+//        environment.tick ();
 
     }
 
-
     public static void main(String[] args) {
-        launch(args);
+        launch (args);
     }
 }
