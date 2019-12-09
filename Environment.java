dca39658e35ae9495f3226e5827254e3d52ef24a
ManOnEdge <mruss797@gmail.com>
2019-12-09 17:09:30 +0000

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -5,19 +5,36 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Environment {
-    int height;
-    int width;
-    public void tick()
-    {
-        while(true){
-            System.out.println ("++");
-            try{
-                Thread.sleep(100);
-            }
-            catch(InterruptedException e){
-                System.out.println(e);
-            }
+
+    private boolean running = false;
+
+    ArrayList<Organism> population = new ArrayList<> ();
+
+    public Environment(int populationSize, int width, int height) {
+        Random rnd = new Random ();
+
+        for (int i = 0; i < populationSize; i++) {
+            int x = rnd.nextInt (width);
+            int y = rnd.nextInt (height);
         }
+    }
+
+    public void tick() {
+
+        System.out.println ("++");
+
         //for loop to update every organism
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void startEnv() {
+        running = true;
+    }
+
+    public void stopEnv() {
+        running = false;
     }
 }
