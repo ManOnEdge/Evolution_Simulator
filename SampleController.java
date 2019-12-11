104e9aa73e16e67daa458be7efdab896fc32d186
ManOnEdge <mruss797@gmail.com>
2019-12-11 15:02:35 +0000

    Update SampleController.java

--- SampleController.java
+++ SampleController.java
@@ -1,7 +1,7 @@
 package sample;
 
-        import javafx.scene.control.Button;
-        import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class SampleController {
     public Button butRun;
@@ -21,12 +21,10 @@ public class SampleController {
 
     public void pressRun() //Activates the simulator
     {
-        /*String OrgNoValue = OrgNo.getText ();
-        String OrgSpeedValue = OrgSpeed.getText();
-        System.out.println (OrgSpeedValue);*/
-        butRun.setOnAction (event -> {
-
-        });
+        int OrgNoValue = Integer.valueOf (OrgNo.getText ());
+        int OrgSizeValue = Integer.valueOf (OrgSize.getText ());
+        Config config = new Config (OrgNoValue,OrgSizeValue);
 
     }
+
 }
