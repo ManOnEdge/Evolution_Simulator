404c012622068fdce5ac7926e117cb166faf2a09
ManOnEdge <mruss797@gmail.com>
2019-12-11 15:00:02 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,38 +14,66 @@ public class Main extends Application {
     public static final int Width = 646;
     public static final int Height = 439;
     public static final int butHeight = 26;
+    private CanvasController canvasController;
+    //private SampleController sampleController;
+    //private Config config;
+
+    /*private static Main instance;
+    public Main() {
+        instance = this;
+    }
+    // static method to get instance of view
+    public static Main getInstance() {
+        return instance;
+    }*/
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        //FXMLLoader sampleLoader = new FXMLLoader (getClass ().getResource ("sample.fxml"));
         GridPane sampleRoot = FXMLLoader.load (getClass ().getResource ("sample.fxml"));
+        //sampleController = sampleLoader.getController ();
         primaryStage.setTitle ("Evolution Simulator");
         Scene sampleScene = new Scene (sampleRoot, 500, 550);
         primaryStage.setScene (sampleScene);
         primaryStage.show ();
+        //sampleController.main = this;
 
         Button butRun = (Button) sampleScene.lookup ("#butRun");
         butRun.setOnAction (event -> {
             try {
+                FXMLLoader canvasLoader = new FXMLLoader (getClass ().getResource ("canvas.fxml"));
                 AnchorPane canvasRoot = FXMLLoader.load (getClass ().getResource ("canvas.fxml"));
-                Scene scene = new Scene (canvasRoot, Width, Height+butHeight);
+                canvasController = canvasLoader.getController ();
+                Scene scene = new Scene (canvasRoot, Width, Height + butHeight);
                 primaryStage.setScene (scene);
                 primaryStage.show ();
+                //canvasController.setConfig(config);
+                //canvasController.initialize1 ();
 
             } catch (IOException e) {
                 e.printStackTrace ();
             }
         });
-
-        /*AnchorPane canvasRoot = FXMLLoader.load (getClass ().getResource ("canvas.fxml"));
-
-        Scene scene = new Scene (canvasRoot, Width, Height);
-        primaryStage.setScene (scene);
-        primaryStage.show ();*/
-//        CanvasController controller = loader.getController ();
-//        Environment environment = new Environment (10, Width, Height);
-//        environment.tick ();
-
     }
+
+
+    @Override
+    public void stop() throws Exception {
+        super.stop ();
+        if (canvasController != null) {
+            canvasController.stop ();
+        }
+
+        System.exit (0);
+    }
+    /*public void setConfig(Config config){
+        this.config = config;
+    }
+
+    public Config getConfig() {
+        return config;
+    }*/
 
     public static void main(String[] args) {
         launch (args);
