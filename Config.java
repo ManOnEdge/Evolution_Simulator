68da5f0fc9acca24b1b10174974f8fc48b9ca695
ManOnEdge <mruss797@gmail.com>
2019-12-11 15:03:51 +0000

    Add files via upload

---
+++ Config.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Config {
+    int orgNo;
+    int orgSize;
+
+    public Config(int orgNo, int orgSize) {
+        this.orgNo = orgNo;
+        this.orgSize = orgSize;
+    }
+}
