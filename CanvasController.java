0386fd0fbf522b4c1077f0ee99feb1ea5d10f7ed
ManOnEdge <mruss797@gmail.com>
2019-12-11 15:00:43 +0000

    Update CanvasController.java

--- CanvasController.java
+++ CanvasController.java
@@ -5,7 +5,6 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class CanvasController {
 
@@ -13,38 +12,23 @@ public class CanvasController {
     public Button butDraw;
     public Environment env;
     public Button butStop;
+    //public Config config;
 
-    public void draw(ArrayList<Organism> population) {
+    public void draw(ArrayList<Organism> population, ArrayList<FoodItem> foods) {
 
         GraphicsContext gc = Plane.getGraphicsContext2D ();
-        Random rnd = new Random ();
-
         gc.clearRect (0, 0, Main.Width, Main.Height);
         for (Organism organism : population) {
             gc.strokeOval (organism.x, organism.y, organism.size, organism.size);
         }
+        for (FoodItem foodItem : foods) {
+            gc.fillRect (foodItem.x, foodItem.y, 2, 2);
+        }
     }
 
     public void pressDraw() {
-        //draw ();
-
-        //env.startEnv ();
         env.startEnv ();
-        //start thread for environment
 
-        /*Runnable envThread = () -> {
-            while (env.isRunning ()) {
-                try {
-                    env.tick ();
-                    draw ();
-                    Thread.sleep (100);
-                } catch (InterruptedException e) {
-                    System.out.println (e);
-                }
-            }
-
-        };
-        new Thread (envThread).start ();*/
     }
 
     public void pressStop() {
@@ -52,7 +36,19 @@ public class CanvasController {
     }
 
     public void initialize() {
-        env = new Environment (3, Main.Width, Main.Height, this);
+        env = new Environment (3, 10, Main.Width, Main.Height, this);
         env.tick ();
     }
+
+    public void stop() {
+        env.stop ();
+    }
+
+    /*public Config getConfig() {
+        return config;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }*/
 }
