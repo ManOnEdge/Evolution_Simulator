79531aca14a0fb30632f6a644f619aaff581a784
ManOnEdge <mruss797@gmail.com>
2020-02-04 12:22:12 +0000

    Update SpeedMutatedOrganism.java

--- SpeedMutatedOrganism.java
+++ SpeedMutatedOrganism.java
@@ -2,8 +2,8 @@ package sample;
 
 public class SpeedMutatedOrganism extends Organism {
 
-    public SpeedMutatedOrganism(int size, int speed, int foodRequired, int x, int y) {
-        super ( size, speed, foodRequired, x, y);
-
+    SpeedMutatedOrganism(int size, double speed, int foodRequired, double x, double y) {
+        super (size, speed, foodRequired, x, y);
     }
+
 }
