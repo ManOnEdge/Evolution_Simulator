1433f505c3074b57446ef11518d8816976b90c9e
ManOnEdge <mruss797@gmail.com>
2020-02-04 12:23:21 +0000

    Update Config.java

--- Config.java
+++ Config.java
@@ -2,10 +2,29 @@ package sample;
 
 public class Config {
     int orgNo;
+    double orgSpeed;
     int orgSize;
+    int orgFood;
+    int speedMONo;
+    double mutSpeed;
+    int sizeMONo;
+    int mutSize;
+    int foodMONo;
+    int mutFoodReq;
+    int foodNo;
 
-    public Config(int orgNo, int orgSize) {
+    public Config(int orgNo, double orgSpeed, int orgSize, int orgFood, int speedMONo, double mutSpeed,
+                  int sizeMONo, int mutSize, int foodMONo, int mutFoodReq, int foodNo) {
         this.orgNo = orgNo;
+        this.orgSpeed = orgSpeed;
         this.orgSize = orgSize;
+        this.orgFood = orgFood;
+        this.speedMONo = speedMONo;
+        this.mutSpeed = mutSpeed;
+        this.sizeMONo = sizeMONo;
+        this.mutSize = mutSize;
+        this.foodMONo = foodMONo;
+        this.mutFoodReq = mutFoodReq;
+        this.foodNo = foodNo;
     }
 }
